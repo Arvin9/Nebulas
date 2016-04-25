859acbcec864a513165982355b5cbec4baf8bd83
nebula <594113869@qq.com>
2016-04-25 17:00:37 +0800

    配置log4j
    配置日志输出

--- src/main/java/site/nebulas/controller/helloController.java
+++ src/main/java/site/nebulas/controller/helloController.java
@@ -1,13 +1,22 @@
 package site.nebulas.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
 @Controller
 public class helloController {
+	private Logger logger = LoggerFactory.getLogger(helloController.class);
+	
 	@RequestMapping("/hello")
 	public String hello(){
 		System.out.println("hello");
+		
+		logger.debug("helllo debug");
+		
 		return "hellomvc";
 	}
 }
