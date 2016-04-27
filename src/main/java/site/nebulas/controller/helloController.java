9632906b3731f7ee4964244e017e3099b5fe2346
Arvin9 <594113869@qq.com>
2016-04-27 08:57:06 +0800

    mybatis配置，映射错误，1。配置文件可能没配好。2。映射出错

--- src/main/java/site/nebulas/controller/helloController.java
+++ src/main/java/site/nebulas/controller/helloController.java
@@ -1,9 +1,13 @@
 package site.nebulas.controller;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import site.nebulas.service.DailySentenceService;
 
 
 
@@ -11,10 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class helloController {
 	private Logger logger = LoggerFactory.getLogger(helloController.class);
 	
+	@Resource 
+	private DailySentenceService dailySentenceService;
+	
+	
 	@RequestMapping("/hello")
 	public String hello(){
-		System.out.println("hello");
-		
+		System.out.println(dailySentenceService.getDailySentenceByParm(null).toString());
+		logger.debug(dailySentenceService.getDailySentenceByParm(null).toString());
 		logger.debug("helllo debug");
 		
 		return "hellomvc";
