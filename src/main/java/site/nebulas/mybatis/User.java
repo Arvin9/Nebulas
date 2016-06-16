01cc861a78b579e7612eeadfc20d9833241993e3
nebula <594113869@qq.com>
2016-06-16 10:16:28 +0800

    mybatis

---
+++ src/main/java/site/nebulas/mybatis/User.java
@@ -0,0 +1,26 @@
+package site.nebulas.mybatis;
+
+public class User {
+	private Integer id;
+	private String userName;
+	private String sex;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+}
