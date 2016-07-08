a97aeb3c4f691c23fc3f2c13c21dbea1d2af3bcc
nebula <594113869@qq.com>
2016-07-08 16:26:36 +0800

    .............

--- src/main/java/site/nebulas/controller/uploadController.java
+++ src/main/java/site/nebulas/controller/uploadController.java
@@ -3,7 +3,16 @@ package site.nebulas.controller;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
+
 import javax.annotation.Resource;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.params.HttpParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,6 +27,8 @@ import site.nebulas.entity.File;
 import site.nebulas.service.DailySentenceService;
 import site.nebulas.service.FileService;
 import site.nebulas.util.DateUtil;
+import site.nebulas.util.Encodes;
+import site.nebulas.util.HttpUtil;
 
 
 
@@ -46,34 +57,39 @@ public class uploadController {
 		return "upload";
 	}
 	
+
 	@RequestMapping("/uploadFile")
 	public String uploadFile(@RequestParam("uploadFile")MultipartFile uploadFile) throws IOException{
 		File file = new File();
 		
+		
+
 		if (uploadFile!=null&&!uploadFile.isEmpty()) {
 			file.setFileName(uploadFile.getOriginalFilename());
 			file.setFileType(uploadFile.getContentType());
 			file.setFileSize(uploadFile.getSize());
-			//file.setFielContent();
+			
 			InputStream input = uploadFile.getInputStream();
-				ByteArrayOutputStream output = new ByteArrayOutputStream();
-				byte[] buffer = new byte[4096];
-				int n = 0;
-			    while (-1 != (n = input.read(buffer))) {
-			        output.write(buffer, 0, n);
-			    }
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			byte[] buffer = new byte[4096];
+			int n = 0;
+			while (-1 != (n = input.read(buffer))) {
+				output.write(buffer, 0, n);
+			}
+			//上传翻译任务
+			
+			String contentBase64 = Encodes.encodeBase64(output.toByteArray());
+			
+			
+		
+			
 			file.setFielContent(output.toByteArray());
 			file.setAddTime(DateUtil.getSysdate(DateUtil.TYPE_DATETIME));    
 			logger.info("uploadFileName:"+file.getFileName());
 			logger.info("uploadFileType:"+file.getFileType());
 			logger.info("uploadFileSize:"+file.getFileSize());
 			
-			try {
-				System.out.println(uploadFile.getInputStream());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 			fileService.insert(file);
 		}
 		logger.debug("uploadFile debug");
