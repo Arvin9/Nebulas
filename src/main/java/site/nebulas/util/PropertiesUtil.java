7ad8f4cb8a58ad619da73d36dd6e0e7e192faf67
nebula <594113869@qq.com>
2016-08-19 10:34:39 +0800

    读取配置文件，分装成map

--- src/main/java/site/nebulas/util/PropertiesUtil.java
+++ src/main/java/site/nebulas/util/PropertiesUtil.java
@@ -1,5 +1,37 @@
 package site.nebulas.util;
 
-public class PropertiesUtil {
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
+public  class PropertiesUtil {
+	
+	/**
+	 * 读取配置文件，分装成map 
+	 * 
+	 **/
+	private static Map<String,String> getProperties() {
+		InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("properties"+File.separator+"test.properties");   
+		  Properties p = new Properties();
+		  Map<String,String> map = new HashMap<String,String>();
+		  try {   
+		   p.load(inputStream);
+		   for(Object key : p.keySet()){
+			   map.put((String)key, p.getProperty((String)key));
+		   }
+		  } catch (IOException e1) {   
+		   e1.printStackTrace();   
+		  }   
+		return map;
+	}
+	
+	public static void main(String[] args) {
+		Map<String,String> map = getProperties();
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+	}
 }
